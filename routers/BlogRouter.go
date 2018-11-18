9a04f5deaddf8a1c6c3f7234cc5f608937465754
aarone <184600228@qq.com>
2018-11-18 22:31:04 +0800

    test

--- routers/BlogRouter.go
+++ routers/BlogRouter.go
@@ -1,12 +1,13 @@
 package routers
 
 import (
-	"github.com/astaxie/beego"
 	"beeblog/controllers"
+	"github.com/astaxie/beego"
 )
 
 func init() {
 	beego.Router("/blog/new", &controllers.BlogController{}, "post:Save")
 	beego.Router("/blog/:id([0-9]+)", &controllers.BlogController{}, "get:Get")
 	beego.Router("/blog/new", &controllers.BlogController{}, "get:New")
+	beego.Router("/blog1", &controllers.BlogController{}, "get:Blog1")
 }
