0a2f26876058bb3de724dd57bffc13d70df4fc89
mail_yanpeng@163.com <yyyq94581017>
2018-12-03 16:57:56 +0800

    note精度问题

--- models/Note.go
+++ models/Note.go
@@ -9,7 +9,7 @@ type Note struct {
 	Id       int64
 	UserId   int64
 	Title    string
-	NoteHtml string    `type(text)"`
+	NoteHtml string    `orm:"type(text)"`
 	Pid      int64
 	Utime    time.Time `orm:"auto_now_add;type(datetime)"`
 }
